69f5796372df9e11cb81d910844ac8a215d1c157
Heloise HAMEL <24438@student.dorset-college.ie>
2021-10-20 15:31:16 +0100

    Ajoutez des fichiers projet.

---
+++ Week 6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Week_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "ici mettre la localisation";
+            string file = "jamaica.txt";
+            string fileToRead = $"{path}/{file}";
+
+            try
+            {
+
+                using (StreamReader srn = new StreamReader(fileToRead))
+                {
+                    
+                    Console.WriteLine($"Starting to Reading {fileToRead}");
+                    string line;
+
+                    while((line = srn.ReadLine()) != null)
+                    {
+                        Console.WriteLine($"\t{line}");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
